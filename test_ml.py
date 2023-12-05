6ae66053927a5042ed79a225de7cec0396facb59
T-Peck <125160301+T-Peck@users.noreply.github.com>
2023-12-05 13:13:16 -0600

    unit test
    unit test ran, with screeenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,66 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
+import numpy as np
+from ml.data import process_data
+from ml.model import (train_model)
+from sklearn.linear_model import LogisticRegression
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_shape():
     """
-    # add description for the first test
+    Does the training and test datasets have the expected shape.
     """
-    # Your code here
+    data= pd.DataFrame({
+        
+            "1": [1,2,3],
+            "2": [1,2,3],
+            "3": [1,2,3],}
+        
+    )
+    assert data.shape == data.dropna().shape
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_algorithm():
     """
-    # add description for the second test
+    Does the ML model uses the expected algorithm.
     """
-    # Your code here
+    data=pd.DataFrame({
+        "1": [1,2,3],
+        "2": [1,1,1],
+        "3": [1,0,1],
+    })
+    X_train, y_train, _, _ = process_data(
+        data,
+        categorical_features=["1"],
+        label="3",
+        training=True,
+    )
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, LogisticRegression)
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_type():
     """
-    # add description for the third test
+    Does any ML functions return the expected type of result.
     """
-    # Your code here
+    data=pd.DataFrame({
+        "1": [1,2,3],
+        "2": [1,1,1],
+        "3": [1,0,1],
+    })
+    X_train, y_train, _, _ = process_data(
+        data,
+        categorical_features=["1"],
+        label="3",
+        training=True,
+    )
+    model = train_model(X_train, y_train)
+    expected_model_type=LogisticRegression
+    assert isinstance(model,expected_model_type)
     pass
